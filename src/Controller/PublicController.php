c0b34ab8088e714bd5c5751bf254c7401074bb1d
signupromain <romain@machaharson.com>
2018-10-17 12:09:02 +0200

    sections

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -23,4 +23,42 @@ class PublicController extends AbstractController
             'articles' => $art,
         ]);
     }
+    /**
+     *
+     * Matches /article/{id},
+     * {id} is a requirement digit: "\d+" for more security ,
+     * to view an article's detail
+     *
+     * @Route("/a/{id}", name="detail_article", requirements={"id"="\d+"})
+     */
+    public function oneArticle($id){
+        $entityManager = $this->getDoctrine()->getManager();
+        $rub = $entityManager->getRepository(Sections::class)->findAll();
+        $art = $entityManager->getRepository(Articles::class)->find($id);
+        return $this->render('public/one_article.html.twig', [
+            'sections' => $rub,
+            'articles' => $art,
+        ]);
+    }
+    /**
+     *
+     * Matches /section/{id},
+     * {id} is a requirement digit: "\d+" for more security ,
+     * to view an article's detail
+     *
+     * @Route("/section/{id}", name="detail_section", requirements={"id"="\d+"})
+     */
+    public function oneSection($id){
+        $entityManager = $this->getDoctrine()->getManager();
+        $rub = $entityManager->getRepository(Sections::class)->findAll();
+        $section = $entityManager->getRepository(Sections::class)->find($id);
+
+        $art = $section->getArticlesarticles();
+
+        return $this->render('public/one_section.html.twig', [
+            'sections' => $rub,
+            'section' => $section,
+            'articles' => $art,
+        ]);
+    }
 }
