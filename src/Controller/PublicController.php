8a28250e85032d14b859546d14d3d52b5a559ad9
signupromain <romain@machaharson.com>
2018-10-18 12:06:45 +0200

    udpate

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -17,7 +17,7 @@ class PublicController extends AbstractController
     {
         $entityManager = $this->getDoctrine()->getManager();
         $rub = $entityManager->getRepository(Sections::class)->findAll();
-        $art = $entityManager->getRepository(Articles::class)->findAll();
+        $art = $entityManager->getRepository(Articles::class)->findBy([],["idarticles"=>"DESC"]);
         return $this->render('public/index.html.twig', [
             'sections' => $rub,
             'articles' => $art,
@@ -54,6 +54,13 @@ class PublicController extends AbstractController
         $section = $entityManager->getRepository(Sections::class)->find($id);
 
         $art = $section->getArticlesarticles();
+
+ /*       $rawSql = "SELECT m.id, (SELECT COUNT(i.id) FROM item AS i WHERE i.myclass_id = m.id) AS total FROM myclass AS m";
+
+        $stmt = $this->getEntityManager()->getConnection()->prepare($rawSql);
+        $stmt->execute([]);
+
+        return $stmt->fetchAll();*/
 
         return $this->render('public/one_section.html.twig', [
             'sections' => $rub,
--- src/Entity/Sections.php
+++ src/Entity/Sections.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App\Entity;
-
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
 /**
  * Sections
  *
@@ -22,28 +19,33 @@ class Sections
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $idsections;
-
     /**
      * @var string
      *
      * @ORM\Column(name="thetitled", type="string", length=80, nullable=false)
      */
     private $thetitled;
-
     /**
      * @var string|null
      *
      * @ORM\Column(name="thedesc", type="string", length=350, nullable=true)
      */
     private $thedesc;
-
     /**
      * @var \Doctrine\Common\Collections\Collection
      *
-     * @ORM\ManyToMany(targetEntity="Articles", mappedBy="sectionssections")
+     * @ORM\ManyToMany(targetEntity="Articles", inversedBy="sectionssections")
+     * @ORM\OrderBy({"idarticles" = "DESC"})
+     * @ORM\JoinTable(name="sections_has_articles",
+     *   joinColumns={
+     *     @ORM\JoinColumn(name="sections_idsections", referencedColumnName="idsections")
+     *   },
+     *   inverseJoinColumns={
+     *     @ORM\JoinColumn(name="articles_idarticles", referencedColumnName="idarticles")
+     *   }
+     * )
      */
     private $articlesarticles;
-
     /**
      * Constructor
      */
@@ -51,36 +53,28 @@ class Sections
     {
         $this->articlesarticles = new \Doctrine\Common\Collections\ArrayCollection();
     }
-
     public function getIdsections(): ?int
     {
         return $this->idsections;
     }
-
     public function getThetitled(): ?string
     {
         return $this->thetitled;
     }
-
     public function setThetitled(string $thetitled): self
     {
         $this->thetitled = $thetitled;
-
         return $this;
     }
-
     public function getThedesc(): ?string
     {
         return $this->thedesc;
     }
-
     public function setThedesc(?string $thedesc): self
     {
         $this->thedesc = $thedesc;
-
         return $this;
     }
-
     /**
      * @return Collection|Articles[]
      */
@@ -88,25 +82,18 @@ class Sections
     {
         return $this->articlesarticles;
     }
-
     public function addArticlesarticle(Articles $articlesarticle): self
     {
         if (!$this->articlesarticles->contains($articlesarticle)) {
             $this->articlesarticles[] = $articlesarticle;
-            $articlesarticle->addSectionssection($this);
         }
-
         return $this;
     }
-
     public function removeArticlesarticle(Articles $articlesarticle): self
     {
         if ($this->articlesarticles->contains($articlesarticle)) {
             $this->articlesarticles->removeElement($articlesarticle);
-            $articlesarticle->removeSectionssection($this);
         }
-
         return $this;
     }
-
 }
