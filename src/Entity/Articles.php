1c4bfd3a7fb6a6a45c0f75bff00446cad11092f3
signupromain <romain@machaharson.com>
2018-10-18 15:50:34 +0200

    crud

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -1,17 +1,14 @@
 <?php
-
 namespace App\Entity;
-
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
-
 /**
  * Articles
  *
  * @ORM\Table(name="articles", indexes={@ORM\Index(name="fk_articles_users_idx", columns={"users_idusers"})})
  * @ORM\Entity
+ *
  */
 class Articles
 {
@@ -23,28 +20,24 @@ class Articles
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $idarticles;
-
     /**
      * @var string
      *
      * @ORM\Column(name="thetitle", type="string", length=200, nullable=false)
      */
     private $thetitle;
-
     /**
      * @var string
      *
-     * @ORM\Column(name="thetext", type="text", length=255, nullable=false)
+     * @ORM\Column(name="thetext", type="text", length=65535, nullable=false)
      */
     private $thetext;
-
     /**
      * @var \DateTime|null
      *
      * @ORM\Column(name="thedate", type="datetime", nullable=true, options={"default"="CURRENT_TIMESTAMP"})
      */
     private $thedate = 'CURRENT_TIMESTAMP';
-
     /**
      * @var \Users
      *
@@ -54,7 +47,6 @@ class Articles
      * })
      */
     private $usersusers;
-
     /**
      * @var \Doctrine\Common\Collections\Collection
      *
@@ -69,68 +61,54 @@ class Articles
      * )
      */
     private $sectionssections;
-
     /**
      * Constructor
      */
     public function __construct()
     {
         $this->sectionssections = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->setThedate(new \DateTime());
     }
-
     public function getIdarticles(): ?int
     {
         return $this->idarticles;
     }
-
     public function getThetitle(): ?string
     {
         return $this->thetitle;
     }
-
     public function setThetitle(string $thetitle): self
     {
         $this->thetitle = $thetitle;
-
         return $this;
     }
-
     public function getThetext(): ?string
     {
         return $this->thetext;
     }
-
     public function setThetext(string $thetext): self
     {
         $this->thetext = $thetext;
-
         return $this;
     }
-
     public function getThedate(): ?\DateTimeInterface
     {
         return $this->thedate;
     }
-
     public function setThedate(?\DateTimeInterface $thedate): self
     {
         $this->thedate = $thedate;
-
         return $this;
     }
-
     public function getUsersusers(): ?Users
     {
         return $this->usersusers;
     }
-
     public function setUsersusers(?Users $usersusers): self
     {
         $this->usersusers = $usersusers;
-
         return $this;
     }
-
     /**
      * @return Collection|Sections[]
      */
@@ -138,23 +116,24 @@ class Articles
     {
         return $this->sectionssections;
     }
-
     public function addSectionssection(Sections $sectionssection): self
     {
         if (!$this->sectionssections->contains($sectionssection)) {
             $this->sectionssections[] = $sectionssection;
+            $sectionssection->addArticlesarticle($this);
         }
-
         return $this;
     }
-
     public function removeSectionssection(Sections $sectionssection): self
     {
         if ($this->sectionssections->contains($sectionssection)) {
             $this->sectionssections->removeElement($sectionssection);
+            $sectionssection->removeArticlesarticle($this);
         }
-
         return $this;
     }
-
+    public function __toString()
+    {
+        return (string) $this->getThetitle();
+    }
 }
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -1,9 +1,6 @@
 <?php
-
 namespace App\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
-
 /**
  * Users
  *
@@ -20,70 +17,57 @@ class Users
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $idusers;
-
     /**
      * @var string
      *
      * @ORM\Column(name="thelogin", type="string", length=80, nullable=false)
      */
     private $thelogin;
-
     /**
      * @var string
      *
      * @ORM\Column(name="thepwd", type="string", length=60, nullable=false, options={"fixed"=true,"comment"="bcrypt"})
      */
     private $thepwd;
-
     /**
      * @var string
      *
      * @ORM\Column(name="therealname", type="string", length=160, nullable=false)
      */
     private $therealname;
-
     public function getIdusers(): ?int
     {
         return $this->idusers;
     }
-
     public function getThelogin(): ?string
     {
         return $this->thelogin;
     }
-
     public function setThelogin(string $thelogin): self
     {
         $this->thelogin = $thelogin;
-
         return $this;
     }
-
     public function getThepwd(): ?string
     {
         return $this->thepwd;
     }
-
     public function setThepwd(string $thepwd): self
     {
         $this->thepwd = $thepwd;
-
         return $this;
     }
-
     public function getTherealname(): ?string
     {
         return $this->therealname;
     }
-
     public function setTherealname(string $therealname): self
     {
         $this->therealname = $therealname;
-
         return $this;
     }
     public function __toString()
     {
-       return (string) $this->getThelogin();
+        return (string) $this->getThelogin();
     }
 }
